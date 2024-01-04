e750d7c347181e901c443634cb04c5e8609d2d9b
Edward0619 <122030726+Edward0619@users.noreply.github.com>
2024-01-05 00:05:23 +0800

    nothing

---
+++ script.js
@@ -0,0 +1,13 @@
+function initMap(){
+    map = new google.maps.Map(document.getELementById('map'), {
+     center: {lat: 1.4036986645295546, lng: 103.90331443412312},
+     zoom: 18,
+     mapId: '87d7a7a1194d523c'
+     });
+    }
+    
+    //1.4036986645295546, 103.90331443412312
+
+    function rentvehicle(){
+        window.open("https://www.3schools.in");
+        }
